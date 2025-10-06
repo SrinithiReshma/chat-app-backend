4022bbffc4bb0b8c8498718b2d121f4fab65c8c9
srinithireshma <srinithireshma@gmail.com>
2025-10-06 15:13:33 +0530

    updated

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const messagesRouter = require('./routes/messages');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
 
 // routes
 app.use('/api/messages', messagesRouter);
